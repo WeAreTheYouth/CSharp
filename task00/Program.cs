812393222a3f2e21f3f958c72848b04efd1309d9
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-20 12:01:58 +0300

    task22 done

---
+++ task00/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+string input = Console.ReadLine(); // ввод
+int number = int.Parse(input); // перевод в число
+int square = number * number; // квадрат
+Console.WriteLine($"Число в квадрате равно {square}");
