f1a8d3a449649f630d6d41d0269b431036b8b277
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-23 19:11:11 +0300

    task33 done

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,18 +1,53 @@
 ﻿// Напишите программу замена элементов массива: положительные элементы замените 
 // на соответствующие отрицательные, и наоборот.
 
-int[] array = new int[8];
+int[] numbers = CreateArray(10);
+PrintArray(numbers);
+Replace(numbers);
+PrintArray(numbers);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int number in array)
+    {
+        Console.Write($"{number} ");
+    }
+    Console.WriteLine();
+}
+
+void Replace(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+
+
+
+/*int[] array = new int[10];
 
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-9, 10);
-   /*  Console.Write($"{array[i]} "); */
 }
 
 foreach (int number in array)
 {
     Console.Write($"{number} ");
 }
+Console.WriteLine();
 
 for (int i = 0; i < array.Length; i++)
 {
@@ -22,4 +57,4 @@ for (int i = 0; i < array.Length; i++)
 foreach (int number in array)
 {
     Console.Write($"{number} ");
-}
+}*/
--- task33/Program.cs
+++ task33/Program.cs
@@ -1,14 +1,36 @@
 ﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 
-int[] array = new int[8];
+int[] array = new int[10];
 
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-9, 10);
-   /*  Console.Write($"{array[i]} "); */
 }
 
-foreach (int number in array)
+foreach (int element in array)
 {
-    Console.Write($"{number} ");
+    Console.Write($"{element} ");
+}
+Console.WriteLine("");
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+bool num = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(number == array[i])
+    {
+        num = true;
+    }
+}
+
+if(num)
+{
+    Console.WriteLine($"{number} есть в массиве");
+}
+else
+{
+    Console.WriteLine($"{number} нет в массиве");
 }
