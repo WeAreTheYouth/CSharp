4279b58e7a96b7def65920d4e38b04f9c427f27f
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-24 20:58:42 +0300

    to 39 done

---
+++ task35/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+
+int[] array = CreateArray(123);
+PrintArray(array);
+Console.WriteLine($"{GetSum(array)} чисел в отрезке от 10 до 99");
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    foreach (int number in array)
+    {
+        Console.Write($"{number} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int GetSum(int[] array)
+{
+    int sum = 0;
+    foreach (int element in array)
+    {
+        if (element > 10 && element < 100)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
---
+++ task37/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int[] array = CreateArray(11);
+PrintArray(array);
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+        Console.Write($"[{String.Join(", ", array)}] ");
+        Console.WriteLine("");
+}
+
+
+int[] newArray = new int[(array.Length + 1) / 2];
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    int j = array.Length - 1 - i;
+    newArray[i] = array[i] * array[j];
+}
+
+Console.Write($"[{String.Join(", ", newArray)}] ");
