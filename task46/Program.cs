11715e536a83f54acaccb1d3431b375559bcbde6
WeAreTheYouth <open8your5mind@gmail.com>
2022-09-25 19:20:22 +0300

    task48 done

---
+++ task46/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[ , ] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+
+
+
+int EnterInt(string promt)
+{
+    Console.Write(promt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j], 3} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ task48/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[ , ] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+
+
+
+int EnterInt(string promt)
+{
+    Console.Write(promt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = i + j;
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j], 3} ");
+        }
+        Console.WriteLine();
+    }
+}
